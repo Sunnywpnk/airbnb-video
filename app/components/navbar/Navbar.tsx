dab9239ff677469bb61c8c3a3d45aa11f84dca1f
Sunny <wichayaporn.pu@ku.th>
2024-04-19 17:15:25 +0700

    Day2

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+//import { Container } from "postcss";
+import Container from "../Container";
+import Logo from "./Logo";
+const Navbar = () => {
+    return (
+    <div className="fixed w-full bg-while z-10 shadow-sm">
+        <div
+            className="
+                py-4
+                border-b-[1px]
+            "
+        >
+            <Container>
+                <div className="
+                        flex
+                        flex-row
+                        items-center
+                        justify-between
+                        gap-3
+                        md:gap-0
+                    "
+                    >
+                        <Logo />
+                    
+                </div>
+            </Container>
+
+        </div>
+       
+    </div>
+    );
+}
+
+export default Navbar;
