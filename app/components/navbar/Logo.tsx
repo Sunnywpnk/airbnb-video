a1b2434f48641df591c4a4b72ec4327b56fc3398
Sunny <wichayaporn.pu@ku.th>
2024-04-19 18:23:34 +0700

    Day2.3

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -8,7 +8,11 @@ const Logo = () => {
 
     return(
         <Image
-           
+        alt="Logo"
+        className="hidden md:block cursor-pointer"
+        height="100"
+        width = "100"
+        src = "/images/logo.png"
         />
     )
 }
